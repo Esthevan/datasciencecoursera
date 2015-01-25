ff88c33f869fc67a88b260583797cf03045ab724
Esthevan <exthevan@gmail.com>
2015-01-25 12:42:20 -0800

    Cousera_Rprogramming

--- HelloWord.md
+++
@@ -1,4 +0,0 @@
-##This is a markdown file
-
-* Testing 1
-* Testing 2
---
+++ coursera.R
@@ -0,0 +1,44 @@
+#Name: Esthevan Augusto Goes Gasparoto                                  DATE: 25/01/2015
+#Country: Brazil
+#Course: R Programming
+
+#----------------------------------------------------------------------------------------------- 
+#Step 1: Function makeCacheMatrix
+#-----------------------------------------------------------------------------------------------
+
+#This function tracks locations within the computer's memory where the inverse of the matrix in 
+#question was already processed, not being necessary to process it again (since it calls the
+#inverse matrix in R memory)
+
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+}
+
+#----------------------------------------------------------------------------------------------- 
+#Step 2: Function cacheSolve
+#-----------------------------------------------------------------------------------------------
+
+## This function checks whether the inverse matrix has been previously calculated, 
+#otherwise, the inverse matrix is calculated and its value is returned.
+
+cacheSolve <- function(x, ...) {
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
+}
